89ba3da7944dc0df095ea9856f2e95da532319f6
JohnWickham <john@wjwickham.com>
2019-07-27 16:31:46 -0400

    Sets up 404 error page

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
-const serverless = require('serverless-http')
+const path = require('path');
 const app = express();
 const port = 1314;
 
 app.use(express.static('static'));
+
 app.get('/posts/:name', function(request, response) {
   response.send("Will load post named: " + request.params.name);
+});
+
+app.use(function(request, response) {
+  response.sendFile(path.join(__dirname + '/static/404.html'));
 });
 
 app.listen(port, function() {
@@ -13,4 +18,3 @@ app.listen(port, function() {
 });
 
 module.exports = app;
-module.exports.handler = serverless(app);
