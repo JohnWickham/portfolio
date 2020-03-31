70a2e9978098c36d0981d658cc9081584fbf7a2f
JohnWickham <john@wjwickham.com>
2020-03-31 19:10:25 -0400

    Serves posts from Markdown as HTML

--- routers/posts.js
+++ routers/posts.js
@@ -1,18 +1,44 @@
-var express = require('express');
-var router = express.Router();
-let path = require('path');
+const express = require('express');
+const router = express.Router();
+const fileSystem = require('fs');
+const path = require('path');
+const markdown = require( "markdown" ).markdown;
 
-let responseOptions = {
-  root: path.join(__dirname, '../static'),
-  dotfiles: 'deny',
-  headers: {
-    'x-timestamp': Date.now(),
-    'x-sent': true
-  }
-};
+router.get('/:name', function(request, response, next) {
+  
+  let fileName = request.params.name + ".md"
+  let relativeFilePath = "./static/posts/" + fileName;
+  let filePath = path.resolve(relativeFilePath);
+  fileSystem.access(filePath, error => {
+    
+    if (error) {
+      console.log(error);
+      next();
+      return;
+    }
 
-router.get('/:name', function(request, response) {
-  response.send("Will load post named: " + request.params.name);
+    outputPostContent(filePath, response, next);
+
+  });
+
 });
+
+function outputPostContent(filePath, response, next) {
+
+  fileSystem.readFile(filePath, function read(error, content) {
+   
+    if (error) {
+      next();
+    }
+    
+    let asString = content.toString();
+    let asHTML = markdown.toHTML(asString);
+
+    response.header("Content-Type", "text/html");
+    response.send(asHTML);
+  
+  });
+
+}
 
 module.exports = router;
---
+++ static/posts/post.asp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{post.title}}</title>
+    <link rel="stylesheet" href="./posts.css" />
+</head>
+
+<body>
+    {{post.content}}
+</body>
+
+</html>
