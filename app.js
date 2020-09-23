82da6267cedf0bd71fbcc653c976284a9d8ccfc7
JohnWickham <john@wjwickham.com>
2020-09-23 12:32:28 +0000

    Add option for environment-defined port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 1314;
+const port = process.env.PORT || 1314;
 
 // Enforce secure connections
 let enforce = require('./middleware/secureConnection');
