fb3e9afc786378d99f66d8887a2108d6feddeb71
JohnWickham <john@wjwickham.com>
2020-09-23 12:23:07 +0000

    Add check against ENFORCE_HTTPS env variable

--- middleware/secureConnection.js
+++ middleware/secureConnection.js
@@ -1,6 +1,6 @@
 function enforceSecureConnection(req, res, next) {
 
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development" && process.env.ENFORCE_HTTPS != "no") {
     return res.redirect('https://' + req.get('host') + req.url);
   }
   
